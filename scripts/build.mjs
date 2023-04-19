19407f9a6149635f465f29d48a0bb26c1b2a3743
Gizmo <cunyu@liuli.lol>
2023-04-20 02:56:02 +0800

    build: remove out_dir before building

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,7 +1,8 @@
-import { readFileSync, writeFileSync } from 'fs'
+import { readFileSync, writeFileSync, rmSync } from 'fs'
 import { execSync } from 'child_process'
 
 const out_dir = 'packages/totp-wasm/dist'
+rmSync(out_dir, { recursive: true, force: true })
 execSync('cargo build --target wasm32-unknown-unknown --release')
 execSync(
   `wasm-bindgen --out-dir ${out_dir} --target web target/wasm32-unknown-unknown/release/totp_wasm.wasm`
@@ -12,5 +13,7 @@ execSync(
 
 const wasm_file = `${out_dir}/totp_wasm_bg.wasm`
 const wasm_data = readFileSync(wasm_file).toString('base64')
-const deno_loader = `// @ts-nocheck\nexport default "${wasm_data}";\n`
+const deno_loader = `// @ts-nocheck
+export default "${wasm_data}";
+`
 writeFileSync(`${out_dir}/wasm_data.js`, deno_loader)
