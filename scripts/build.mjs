0e118ab2ffa6ec0cc9aa643e523daf6f78dc59f6
Gizmo <cunyu@liuli.lol>
2023-05-25 15:56:11 +0800

    build: optimize wasm file size

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -8,7 +8,7 @@ execSync(
   `wasm-bindgen --out-dir ${out_dir} --target web target/wasm32-unknown-unknown/release/totp_wasm.wasm`
 )
 execSync(
-  `node_modules/.bin/wasm-opt -Os -o ${out_dir}/totp_wasm_bg.wasm ${out_dir}/totp_wasm_bg.wasm`
+  `node_modules/.bin/wasm-opt -Os --strip-debug --strip-producers --zero-filled-memory -o ${out_dir}/totp_wasm_bg.wasm ${out_dir}/totp_wasm_bg.wasm`
 )
 
 const wasm_file = `${out_dir}/totp_wasm_bg.wasm`
